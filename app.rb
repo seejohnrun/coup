999e6223655522fc6a448f77bc3e2d630973d39d
Eric Tuvesson <eric.tuvesson@gmail.com>
2019-06-23 16:19:50 +0200

    Move more game logic to the game class

--- app.rb
+++ app.rb
@@ -10,73 +10,62 @@ GAMES = Hash.new { |h, k| h[k] = Game.new }
 set :content_type, :json
 set :public_folder, 'public'
 
-def game_for(game, player)
-  {
-    me: player.token,
-    game_over: player.game_over?,
-    deck_size: game.cards.count,
-    hand: {
-      cards: player.cards.map { |c| { type: c.type, token: c.token, lost: c.lost } },
-      money: player.money
-    },
-    players: game.players.values.reject { |p| p == player || p.cards.empty? }.map do |p|
-      {
-        token: p.token,
-        game_over: p.game_over?,
-        money: p.money,
-        card_types: p.cards.select(&:lost).map(&:type)
-      }
-    end
-  }
-end
-
 get '/' do
   redirect '/index.html'
 end
 
 # Reset the game
 post '/games/:game_id/players/:player_id/reset' do
-  game = GAMES[params[:game_id]]
-  player = game.players[params[:player_id]]
+  game    = GAMES[params[:game_id]]
+  player  = game.players[params[:player_id]]
+
   game.reset!
-  game_for(game, player).to_json
+  game.to_json(player)
 end
 
 # Draw a card
 post '/games/:game_id/players/:player_id/draw' do
-  game = GAMES[params[:game_id]]
-  player = game.players[params[:player_id]]
-  params[:cards].to_i.times.each { game.draw_for(player) }
-  game_for(game, player).to_json
+  game    = GAMES[params[:game_id]]
+  player  = game.players[params[:player_id]]
+  count   = params[:cards].to_i
+  
+  game.draw_for(player, count)
+  game.to_json(player)
 end
 
 # Return a card
 post '/games/:game_id/players/:player_id/return/:card_token' do
-  game = GAMES[params[:game_id]]
-  player = game.players[params[:player_id]]
+  game    = GAMES[params[:game_id]]
+  player  = game.players[params[:player_id]]
+
   game.return_from(player, params[:card_token])
-  game_for(game, player).to_json
+  game.to_json(player)
 end
 
 # Refresh game
 get '/games/:game_id/players/:player_id' do
-  game = GAMES[params[:game_id]]
-  player = game.players[params[:player_id]]
-  game_for(game, player).to_json
+  game    = GAMES[params[:game_id]]
+  player  = game.players[params[:player_id]]
+
+  game.to_json(player)
 end
 
 # Adjust money
 post '/games/:game_id/players/:player_id/adjust_money/:amount' do
-  game = GAMES[params[:game_id]]
-  player = game.players[params[:player_id]]
-  player.adjust_money(params[:amount].to_i)
-  game_for(game, player).to_json
+  game    = GAMES[params[:game_id]]
+  player  = game.players[params[:player_id]]
+  amount  = params[:amount].to_i
+
+  player.adjust_money(amount)
+  game.to_json(player)
 end
 
 # Lose influence
 post '/games/:game_id/players/:player_id/lose/:card_token' do
-  game = GAMES[params[:game_id]]
-  player = game.players[params[:player_id]]
-  player.lose_influence(params[:card_token])
-  game_for(game, player).to_json
+  game    = GAMES[params[:game_id]]
+  player  = game.players[params[:player_id]]
+  card    = params[:card_token]
+  
+  player.lose_influence(card)
+  game.to_json(player)
 end
--- coup/game.rb
+++ coup/game.rb
@@ -13,9 +13,9 @@ class Game
   end
 
   # Draw a card (shuffle first)
-  def draw_for(player)
+  def draw_for(player, count = 1)
     @cards.shuffle!
-    player.cards << @cards.pop
+    count.times { player.cards << @cards.pop }
   end
 
   def return_from(player, card_token)
@@ -27,5 +27,25 @@ class Game
   def reset!
     @cards = (CARD_TYPES * 3).map { |c| Card.new(c) }
     @players = Hash.new { |h, k| h[k] = Player.new(self, k) }
+  end
+
+  def to_json(player)
+    {
+      me: player.token,
+      game_over: player.game_over?,
+      deck_size: cards.count,
+      hand: {
+        cards: player.cards.map { |c| { type: c.type, token: c.token, lost: c.lost } },
+        money: player.money
+      },
+      players: players.values.reject { |p| p == player || p.cards.empty? }.map do |p|
+        {
+          token: p.token,
+          game_over: p.game_over?,
+          money: p.money,
+          card_types: p.cards.select(&:lost).map(&:type)
+        }
+      end
+    }.to_json
   end
 end
